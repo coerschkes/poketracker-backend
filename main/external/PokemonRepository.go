c49f0e7735d538bf8006f16c9477658ccca157e1
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-23 16:46:41 +0200

    feat: Implement pseudo code for the pokemon repository.

--- main/external/PokemonRepository.go
+++ main/external/PokemonRepository.go
@@ -18,19 +18,45 @@ func NewPokemonRepositoryImpl() *PokemonRepositoryImpl {
 }
 
 func (i *PokemonRepositoryImpl) FindAll() []domain.Pokemon {
-	//todo: implement
+	/*
+		todo:
+			* check if user exists
+			* retrieve all pokemon where user id is equal to the current userid
+			* lazy load pokemon edition relation
+			* return pokemon slice
+	*/
 	return nil
 }
 
 func (i *PokemonRepositoryImpl) Create(pokemon domain.Pokemon) {
-	//todo: implement
+	/*
+		todo:
+			* check if user exists
+			* check if pokemon exists for user
+			* create pokemon edition relation
+			* return
+	*/
 }
 
 func (i *PokemonRepositoryImpl) Delete(id int) {
-	//todo: implement
+	/*
+		todo:
+			* check if user exists
+			* check if pokemon exists for user
+			* delete pokemon edition relation
+			* delete pokemon
+			* return
+	*/
 }
 
 func (i *PokemonRepositoryImpl) Find(id int) domain.Pokemon {
-	//todo: implement
+	/*
+		todo:
+			* check if user exists
+			* check if pokemon exists for user
+			* load pokemon
+			* load pokemon edition relation
+			* return
+	*/
 	return domain.Pokemon{}
 }
